eb400fb3e05c51b7141e2417071d44d65025ceee
Olusanya Timothy <seunxplicit@gmail.com>
2022-03-06 11:03:26 +0100

    package modified

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ class App {
   constructor(routes: Routes[]) {
     this.app = express();
     this.env = NODE_ENV || 'development';
-    this.port = PORT || 3000;
+    this.port = process.env.PORT || 3000;
 
     this.connectToDatabase();
     this.initializeMiddlewares();
