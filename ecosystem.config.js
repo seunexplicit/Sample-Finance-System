b28459283f4d8cd7b4e4bf5947e3a5de8dc020ae
Olusanya Timothy <seunxplicit@gmail.com>
2022-03-06 11:50:34 +0100

    Heroku

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,12 +14,13 @@
       instances: 2, // pm2 instance count
       autorestart: true, // auto restart if process crash
       watch: false, // files change automatic restart
-      ignore_watch: ['node_modules', 'logs'], // ignore files change
+      watch: ['dist'],
+      ignore_watch: ['logs'], // ignore files change
       max_memory_restart: '1G', // restart if process use more than 1G memory
       merge_logs: true, // if true, stdout and stderr will be merged and sent to pm2 log
       output: './logs/access.log', // pm2 log file
       error: './logs/error.log', // pm2 error log file
-      env: { // environment variable
+      env_production: { // environment variable
         NODE_ENV: 'production',
       },
     },
@@ -38,7 +39,6 @@
       output: './logs/access.log', // pm2 log file
       error: './logs/error.log', // pm2 error log file
       env: { // environment variable
-        PORT: 3000,
         NODE_ENV: 'development',
       },
     },
